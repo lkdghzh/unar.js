e1f7a00a0c5980cb6f3eab659f16a703b6d10029
lkdghzh <likesite@foxmail.com>
2018-01-12 14:34:26 +0800

    filter pure white space Text node

--- src/View/index.js
+++ src/View/index.js
@@ -1,24 +1,43 @@
 import Attr from "./attr"
 import Detective from "./Detective"
-
-class Templater {
-    constructor(selector,vm) {
-        this.vm=vm;
+debugger
+export default class Templater {
+    constructor(selector, vm) {
+        debugger
+        this.vm = vm;
         this.el = document.querySelector(selector);
         if (this.el) {
-            this.ele.appendChild(this.init());
+            this.el.appendChild(this.init());
         }
     }
     init() {
         const fragment = document.createDocumentFragment();
+        var waitDeletedNode;
         //所有节点node
-        for (let node of this.el.childNodes) {
-            fragment.appendChild(node);
+        //node appendChild之前会被删除，影响原始数据
+        //   for (let [inx,node] of this.el.childNodes.entries()) {
+        //     const nodeType = node.nodeType;
+        //     if (nodeType === 1 || nodeType === 8) {
+        //         fragment.appendChild(node);
+        //         inx=inx-1;
+        //     }
+        // }
+        for (let i = 0; i < this.el.childNodes.length; i++) {
+            const node = this.el.childNodes[i];
+            const nodeType = node.nodeType;
+            //for reduce the loop count ,filt nodes to Element Comment Text(not contain pure whitespace)
+            if (nodeType === 1 || nodeType === 8 || (nodeType === 3) && !this._isPureBlankNode(node)) {
+                fragment.appendChild(node);
+                i = i - 1;
+            }
         }
-        //fragment.innerHTML=this.el.innerHTML;
+        // while (waitDeletedNode = this.el.firstChild) {
+        //     fragment.appendChild(waitDeletedNode); //隐式删除waitDeletedNode节点
+        // }
         for (let node of fragment.childNodes) {
             this._initFragment(node)
         }
+        return fragment
     }
     _initFragment(parentNode) {
         for (let node of parentNode.childNodes) {
@@ -27,19 +46,19 @@ class Templater {
                 //node.attributes
                 for (let attr of node.attributes) {
                     //过滤掉非unar的动作、属性、事件
-                    const attrName=attr.name;
-                    const attrVal=attr.value;
+                    const attrName = attr.name;
+                    const attrVal = attr.value;
                     //u-html u-model
                     if (Attr.isAction(attrName)) {
-                        Detective[attrName](node,attrVal)
+                        Detective[attrName](node, attrVal)
                     }
                     //:id
                     if (Attr.isProp(attrName)) {
-                        Detective.bind(node,attrName,attrVal)
+                        Detective.bind(node, attrName, attrVal)
                     }
                     //@click
                     if (Attr.isEvt(attrName)) {
-                        Detective.addEvt(node,attrName,attrVal)
+                        Detective.addEvt(node, attrName, attrVal)
                     }
                 }
                 if (node.childNodes.length) {
@@ -47,8 +66,28 @@ class Templater {
                 }
             }
             if (node.nodeType === 3 && Attr.isExpression(attr)) {
-                
+
             }
         }
+    }
+    /**
+     * https://developer.mozilla.org/zh-CN/docs/Web/Guide/API/DOM/Whitespace_in_the_DOM
+     * 以下所谓的“空白符”代表：
+     *  "\t" TAB \u0009 （制表符）
+     *  "\n" LF  \u000A （换行符）
+     *  "\r" CR  \u000D （回车符）
+     *  " "  SPC \u0020 （真正的空格符）
+     *
+     * 不包括 JavaScript 的“\s”，因为那代表如不断行字符等其他字符。
+     */
+    /**
+     * 测知某节点的文字内容是否全为空白。
+     *
+     * @参数   nod  |CharacterData| 类的节点（如  |Text|、|Comment| 或 |CDATASection|）。
+     * @传回值      若 |nod| 的文字内容全为空白则传回 true，否则传回 false。
+     */
+    _isPureBlankNode(node) {
+        // Use ECMA-262 Edition 3 String and RegExp features
+        return !(/[^\t\n\r ]/.test(node.data));
     }
 }
