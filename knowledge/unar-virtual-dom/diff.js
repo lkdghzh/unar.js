2dff47abfdb52f36efcc015b6f8f62c373b32266
lkdghzh <likesite@foxmail.com>
2018-02-01 18:07:35 +0800

    diff dfs algorithm  mark index

--- knowledge/unar-virtual-dom/diff.js
+++ knowledge/unar-virtual-dom/diff.js
@@ -1,7 +1,18 @@
 /**
  * Get the difference between the two virtual dom
+ * param a :old virtual dom
+ * param b :new virtual dom
+ * return patch:the difference
  */
-export const diff = (oldEl, newEL) => {
-    let patch = {}
-    return patch
+
+ /**
+  * use dfs algorithm
+  * mark for every node width inx 0 1 2 ...
+  */
+const dfs=(a,b,inx,patches)=>{
+    
+}
+export const diff = (a, b) => {
+    let patches = {}
+    return patches
 }
