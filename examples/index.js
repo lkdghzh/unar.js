2f79018c275c02a9fcf697bf5fce670cbc99c395
lkdghzh <likesite@foxmail.com>
2018-02-08 22:46:38 +0800

    test parcel success !

--- examples/index.js
+++ examples/index.js
@@ -1,15 +1,15 @@
-// import Unar from "../src/Instance/index.js"
-// const app = new Unar({
-//     el: "#root",
-//     data: {
-//         a: 1,
-//         id: 'input1'
-//     },
-//     methods: {
-//         fn() {
-//             console.log(this.a)
-//         }
-//     }
-// })
-import {p} from "../parcel.js"
-p.es6fn('like')
+import Unar from "../src/Instance/index.js"
+const app = new Unar({
+    el: "#root",
+    data: {
+        a: 1,
+        id: 'input1'
+    },
+    methods: {
+        fn() {
+            console.log(this.a)
+        }
+    }
+})
+// import {p} from "./parcel.js"
+// p.es6fn('like')
