6baa6103591f948827e32c275d9d758ee286bf32
lkdghzh <likesite@foxmail.com>
2018-02-26 14:03:04 +0800

    Reduce the coupling, separation of agents and monitoring

---
+++ src/Instance/config.js
@@ -0,0 +1,45 @@
+import Register from "../Bll/register"
+export const proxy = (data,vm) => {
+	//创建代理（存取）属性，代理_data、$options对象下的存取属性
+	Object.keys(data).forEach(key => {
+		Object.defineProperty(vm, key, {
+			configurable: false,
+			enumerable: true,
+			get() {
+				//data[key]、this._data[key]、this.$options.data 、o.data都可以
+				//实现代理对象（读写都要通过这个代理），然后访问this._data[key]、this.$options.data的存取器属性
+				//此getter会调用下面getter
+				return data[key]
+			},
+			set(newVal) {
+				data[key] = newVal
+			}
+		})
+		//使用闭包，存储这个值。（data[key],运行到此还是数据属性）
+		//data[key]、this._data[key]、this.$options.data 、o.data都可以
+		var valCache = data[key]
+
+		//data相当于同时，把this_data和this.$options.data,o.data三个都变成了存取器属性
+		Object.defineProperty(data, key, {
+			configurable: false,
+			enumerable: true,
+			get() {
+				//可以尝试这个
+				//this._data[key]，this.$options.data[key]和data[key]都会爆栈
+				return valCache
+			},
+			set(newVal) {
+				console.log(key);
+				valCache = newVal
+				//set value first ，then notify dom update with newVal
+				hubs[key].notify()
+			}
+		})
+	})
+}
+export const watch = (watchers,vm) => {
+	//Object.entries({a:1})-->[["a", 1]]
+	for (let [key, cb] of Object.entries(watchers)) {
+		Register.registListener4Hubs(key, cb, vm)
+	}
+}
--- src/Instance/index.js
+++ src/Instance/index.js
@@ -2,29 +2,9 @@
  * Unar
  */
 
-
-// import Util from "../Util"
-// import detective from "./Detective"
-// import model from "./Model"
-import {
-	hubs
-} from "../Hub"
-import Register from "../Bll/register"
-
+import {proxy,watch} from "./config";
 import Templater from "../View/index.js"
 class Unar {
-	/*options
-	methods{
-	    fn1,
-	    fn2
-	}
-	data{a:1,b:2}
-
-	a:1
-	b:2
-	fn1,
-	fn2
-	*/
 	// static hubs = []
 	constructor({
 		el = '',
@@ -34,52 +14,18 @@ class Unar {
 	}) {
 		this.methods = methods
 		this.$options = {}
-		//在vm新建_data、$options属性
+
+		//this add keys(_data、$options )
 		var data = this._data = this.$options.data = data
 
-		//创建代理（存取）属性，代理_data、$options对象下的存取属性
-		Object.keys(data).forEach(key => {
-			Object.defineProperty(this, key, {
-				configurable: false,
-				enumerable: true,
-				get() {
-					//data[key]、this._data[key]、this.$options.data 、o.data都可以
-					//实现代理对象（读写都要通过这个代理），然后访问this._data[key]、this.$options.data的存取器属性
-					//此getter会调用下面getter
-					return data[key]
-				},
-				set(newVal) {
-					data[key] = newVal
-				}
-			})
-			//使用闭包，存储这个值。（data[key],运行到此还是数据属性）
-			//data[key]、this._data[key]、this.$options.data 、o.data都可以
-			var valCache = data[key]
+		//hijack properties
+		proxy(data,this)
 
-			//data相当于同时，把this_data和this.$options.data,o.data三个都变成了存取器属性
-			Object.defineProperty(data, key, {
-				configurable: false,
-				enumerable: true,
-				get() {
-					//可以尝试这个
-					//this._data[key]，this.$options.data[key]和data[key]都会爆栈
-					return valCache
-				},
-				set(newVal) {
-					console.log(key)
-					valCache = newVal
-					//set value first ，then notify dom update with newVal
-					hubs[key].notify()
-				}
-			})
-		})
+		//add watchers
 		this.watchers = watchers
-		//Object.entries({a:1})-->[["a", 1]]
-		for (let [key, cb] of Object.entries(watchers)) {
-			Register.registListener4Hubs(key, cb, this)
-		}
+		watch(this.watchers,this)
 
-		// Util.initWatcher(this.watchs)
+		//compile fragment
 		new Templater(el, this)
 	}
 	// $watch() {}
@@ -94,7 +40,6 @@ class Unar {
 	// }
 	// $created() {}
 	// $mounted() {}
-
 	// $computed() {}
 	// $remove() {}
 	// $destroy() {}
