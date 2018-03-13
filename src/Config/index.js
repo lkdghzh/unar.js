6be1b5a833cf40c84c74f896fff0a373b024241e
lkdghzh <likesite@foxmail.com>
2018-03-13 16:51:40 +0800

    test config

--- src/Config/index.js
+++ src/Config/index.js
@@ -5,6 +5,11 @@
 // }
 
 //merge
+// import {a} from "../Instance"
+// console.log(a)
+import {default as f,a} from "./test.js"
+console.log(f,a)
+
 var config = {
     actionPrefix: "u",
     attrPrefix: ":",
---
+++ src/Config/test.js
@@ -0,0 +1,11 @@
+import C from './index.js'
+console.log(C)
+
+export var a = "1"
+
+class b {
+	constructor() {
+		this.props = '2'
+	}
+}
+export default b
