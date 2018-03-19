4d246f4d9c05c21f8ee802b2e45d9e361998c42a
lkdghzh <likesite@foxmail.com>
2018-03-20 01:58:59 +0800

    scopeExp run to run expression

--- src/Bll/register.js
+++ src/Bll/register.js
@@ -5,6 +5,9 @@ import {
 	hubs,
 	Hub
 } from "../Hub"
+import {
+	run
+} from '../Utils'
 let count = 0
 export default class Register {
 	static registDomListener4Hubs(node, prop, key, vm, preTxt, nxtTxt) {
@@ -12,7 +15,7 @@ export default class Register {
 			//count several nodes ->same computed prop
 			propType.switch = key + '$' + ++count
 			const ccb = () => {
-				DomFn.bind(node, prop, preTxt + vm[key] + nxtTxt)
+				DomFn.bind(node, prop, preTxt + run(key,vm) + nxtTxt)
 			}
 			propType[key + '$' + count] = ccb
 			ccb()
@@ -21,8 +24,9 @@ export default class Register {
 			const cb = (val, oldVal) => {
 				DomFn.bind(node, prop, preTxt + val + nxtTxt, preTxt + oldVal + nxtTxt)
 			}
-			console.log(`初始化页面，${key}`)
-			cb(vm[key])
+			//'a +"b" === ? "sth" :b'.replace(/[\"]?(\w+)[\"]?/g,(e)=>{console.log(e);return e.startsWith('"')&&e.endsWith('"')||e.startsWith("'")&&e.endsWith("'")?e:'scope.'+e})
+			console.log(`初始化页面，${key}?????${run(key,vm)}`)
+			cb(run(key,vm))
 			this.registListener4Hubs(key, cb, vm)
 		}
 	}
--- src/Utils/index.js
+++ src/Utils/index.js
@@ -34,6 +34,22 @@
 // 		set(newVal) {}
 // 	})
 // }
+
+
+//'{{a+"b"}} c {{d+f}}e'
+// -> scope.a + "b" + " c " + scope.d + scope.f
+// const bind=(exp,scope)=>{
+// 	var result=''
+// 	var parts=exp.split(/\{\{(.+?)\}\}/g)
+// 	return 
+// }
+// a +"b" === ? "sth" :b
+// scope.a +"b" === ? "sth" :scope.b
+// export const computeExp = (exp, scope) => {
+// 	exp = addScope(scope);
+// 	var fn = new Function('scope', 'return ' + exp);
+// 	return fn(scope);
+// }
 export const getPathVal = (obj, path) => {
 	// var o = { a: { b: { c: 1 } } }
 	// getPathVal(o, 'a.b.c')
@@ -51,4 +67,31 @@ export const getPathVal = (obj, path) => {
 export const typeOf = (o) => {
 	var _target
 	return ((_target = typeof (o)) == "object" ? Object.prototype.toString.call(o).slice(8, -1) : _target).toLowerCase()
+}
+
+
+// export const excute = (exp, scope) => {
+// 	try {
+// 		with(scope) {
+// 			return eval(exp)
+// 		}
+// 	} catch (e) {
+// 		console.error('translate exp abnormal')
+// 	}
+// }
+
+// a +"b" === ? "sth" :b
+// scope.a +"b" === ? "sth" :scope.b
+const scopeExp = (exp, vm) =>
+	exp.replace(/[\"]?(\w+)[\"]?/g, (e) =>
+		e.startsWith('"') && e.endsWith('"') ||
+		e.startsWith("'") && e.endsWith("'") ? e : (vm + '.' + e)
+	)
+
+export const run = (exp, scope) => {
+	var body = scopeExp(exp, 'vm')
+	debugger
+	console.log(body, '------??')
+	var fn = new Function('vm', 'return ' + body)
+	return fn(scope)
 }
