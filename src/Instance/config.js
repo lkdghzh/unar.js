c6f93a154ef6087a233fc2229da0c7d0fc67065e
lkdghzh <likesite@foxmail.com>
2018-03-26 13:27:59 +0800

    reduce coupling

--- src/Instance/config.js
+++ src/Instance/config.js
@@ -11,61 +11,67 @@ import {
 export const config = (configs, vm) => {
 	vm.configs = Object.assign(defaultConfigs, configs)
 }
+const hijack = (data, key, vm) => {
+	Object.defineProperty(vm, key, {
+		get() {
+			//it can replaced by this._data[key],this.$options.data[key] ,o.data[key]
+			//not allow valCache
+			//this will call get above
+			return data[key]
+		},
+		set(newVal) {
+			data[key] = newVal
+		}
+	})
+}
+const accessor = (data, key, vm) => {
+	//Data properties->data[key]
+	//it's cached,data[key] can replaced by vm._data[key],vm.$options.data,o.data 
+	var valCache = data[key]
+	//Accessor properties
+	//data reference->At the same time, vm._data ,vm.$options.data, o.data become three accessor properties
+	Object.defineProperty(data, key, {
+		get() {
+			//vm._data[key],vm.$options.data[key],data[key]
+			//maximum call stack size exceeded
+
+			//computeds
+			if (propType.switch) {
+				var currentComputedType = propType.switch
+				console.log(`↑  computed->ccb run,${propType.switch}用到了${key}，向${key}注册${propType.switch}函数`)
+				var cfn = function () {
+					//node fn
+					propType[currentComputedType]()
+					var ckey = currentComputedType.split('$')[0]
+					console.log(`----------${currentComputedType},${ckey}-----------`)
+					//pure computed fn
+					typeOf(vm.computeds[ckey]) === "function" ? vm.computeds[ckey].call(vm) :
+						typeOf(vm.computeds[ckey]) === "object" ? vm.computeds[ckey].get.call(vm) :
+						""
+				}
+				Register.registListener4Hubs(key, cfn, vm)
+			}
+			return valCache
+		},
+		set(newVal) {
+			valCache = newVal
+			//object 
+			// if (typeOf(newVal) === 'object') {
+			// 	proxy(newVal, vm)
+			// }
+			//array
+			// if (typeOf(newVal) === 'array') {
+			// 	// TODO observe array
+			// }
+			//set value first,then notify dom update with newVal
+			hubs[key].notify()
+		}
+	})
+}
 export const proxy = (data, vm) => {
 	Object.keys(data).forEach(key => {
-		//Data properties->data[key]
-		//it's cached,data[key] can replaced by vm._data[key],vm.$options.data,o.data 
-		var valCache = data[key]
-
-		//Accessor properties
-		//data reference->At the same time, vm._data ,vm.$options.data, o.data become three accessor properties
-		Object.defineProperty(data, key, {
-			configurable: false,
-			enumerable: true,
-			get() {
-				//vm._data[key],vm.$options.data[key],data[key]
-				//maximum call stack size exceeded
-
-				//computeds
-				console.log(key)
-
-				if (propType.switch) {
-					var currentComputedType = propType.switch
-					console.log(`↑  computed->ccb run,${propType.switch}用到了${key}，向${key}注册${propType.switch}函数`)
-					var cfn = function () {
-						//node fn
-						propType[currentComputedType]()
-						var ckey = currentComputedType.split('$')[0]
-						console.log(`----------${currentComputedType},${ckey}-----------`)
-						//pure computed fn
-						typeOf(vm.computeds[ckey]) === "function" ?
-							vm.computeds[ckey].call(vm) : typeOf(vm.computeds[ckey]) === "object" ?
-							vm.computeds[ckey].get.call(vm) : ""
-					}
-					Register.registListener4Hubs(key, cfn, vm)
-				}
-				return valCache
-			},
-			set(newVal) {
-				valCache = newVal
-				//set value first,then notify dom update with newVal
-				hubs[key].notify()
-			}
-		})
-
-		Object.defineProperty(vm, key, {
-			configurable: false,
-			enumerable: true,
-			get() {
-				//it can replaced by this._data[key],this.$options.data[key] ,o.data[key]
-				//not allow valCache
-				//this will call get above
-				return data[key]
-			},
-			set(newVal) {
-				data[key] = newVal
-			}
-		})
+		accessor(data, key, vm)
+		hijack(data, key, vm)
 	})
 }
 export const watch = (watchers, vm) => {
