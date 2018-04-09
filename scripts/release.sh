efaf3d68eb9a5f6fb21749d0c8d95458e4d33f91
lkdghzh <likesite@foxmail.com>
2018-04-09 16:22:35 +0800

    npm version eg.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 # if [ -z $0 ];then
 # fi
+currentVersion=$(npm view unar version)
+echo "Current Unar Version is:"$currentVersion
 
-currentVersion=$(node scripts/current-unar-version.js -major)
-echo $currentVersion
-# read -p "release major version ? (y/n)" -n 1
-# majorVersion=$REPLY
-# read -p "are you sure release version $majorVersion ? (y/n)"
-# if [ "$REPLY" == "y" || "$REPLY" == "Y"]
-#     then
-#     echo "release"
-# fi
+read -p "Releasing version, choose from [major | minor | patch | premajor | preminor | prepatch | prerelease | from-git]" -n 1
+version=$REPLY
+case  $version in
+    major) npm version major
+    ;;
+    minor) npm version minor
+    ;;
+    patch) npm version patch
+    ;;
+    premajor) npm version premajor
+    ;;
+    preminor) npm version preminor
+    ;;
+    prepatch) npm version prepatch
+    ;;
+    prerelease) npm version prerelease
+    ;;
+    from-git) npm version from-git
+    ;;
+    *) echo 'You need select a option from chioces'
+    ;;
+nextVersion=$(npm view unar version)
+echo 'Unar Version has changed to:'$nextVersion
+
+read -p "are you sure release version $majorVersion ? (y/n)"
+if [ "$REPLY" == "y" || "$REPLY" == "Y"]
+    then
+    echo "release"
+fi
