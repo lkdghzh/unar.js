7ba12848e3864345d3ef0654543594cb9663e078
lkdghzh <likesite@foxmail.com>
2018-04-29 23:33:54 +0800

    for

--- packages/utils/view.js
+++ packages/utils/view.js
@@ -1,3 +1,14 @@
 export const nullify = (...args) => {
 	args.map(arg => null)
 }
+const createPlaceHolder = () => {
+	return document.createTextNode('')
+}
+const removeChild = (parent, node) => {
+	return parent.removeChild(node)
+}
+
+let Dom = Object.create(null)
+Dom.createPlaceHolder = createPlaceHolder
+Dom.removeChild = removeChild
+export default Dom
--- packages/view/directives/for.js
+++ packages/view/directives/for.js
@@ -5,7 +5,14 @@ export default class For extends Base{
         super(opts)
     }
     bind() {
-
+        // var itemName = exp.split('of')[0].replace(/\s/g, '')
+        // var arrNames = exp.split('of')[1].replace(/\s/g, '').split('.')
+        // var parentNode = node.parentNode
+		// var startNode = document.createTextNode('')
+		// var endNode = document.createTextNode('')
+		// var range = document.createRange()
+		// parentNode.replaceChild(endNode, node) 
+		// parentNode.insertBefore(startNode, endNode)
         super.bind(cb)
     }
 }
