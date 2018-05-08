9a781c2ba47ba756ff6085aa2b2ee6e87c36834e
lkdghzh <likesite@foxmail.com>
2018-05-08 11:59:26 +0800

    fine-grained limit callback function execution time (after temp listener is null)

--- packages/bll/register.js
+++ packages/bll/register.js
@@ -7,8 +7,12 @@ export default class Register {
 		// console.log(`初始化页面get：${exp}`)
 		temp.listener = new Listener(vm, exp, cb)
 		var newVal = run(exp, vm)
-		debugger
-		cb(newVal)
 		temp.listener = null
+		/*
+		 * During callback process, the template may be recompiled ,for directive eg.
+		 * this's time temp.listener is not null, it will be registered to recompiled dom ,hub's listener
+		 * need fine-grained limit callback function execution time (after temp listener is null)
+		 */
+		cb(newVal)
 	}
 }
